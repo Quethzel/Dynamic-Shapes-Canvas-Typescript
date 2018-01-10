f61a356e486467edd8be4fa45d13e3b79b33dc3c
Quethzel <quethzel@gmail.com>
2018-01-10 15:26:33 -0600

    generación de figuras
    se generan las 4 figuras geometricas que se requieren.

--- dynamicShapes.js
+++ dynamicShapes.js
@@ -23,9 +23,10 @@ var Triangle = /** @class */ (function () {
     return Triangle;
 }());
 var Star = /** @class */ (function () {
-    function Star(x, y, outerRadio, innerRadio) {
+    function Star(x, y, spikes, outerRadio, innerRadio) {
         this.cooX = x;
         this.cooY = y;
+        this.spikes = spikes;
         this.outerR = outerRadio;
         this.innerR = innerRadio;
     }
@@ -58,6 +59,7 @@ var Shape = /** @class */ (function () {
 }());
 var Stage = /** @class */ (function () {
     function Stage(canvas, ctx) {
+        this.lineWidth = 8;
         this.canvas = canvas;
         this.ctx = ctx;
         this.cooX = this.canvas.width / 2;
@@ -73,6 +75,54 @@ var Stage = /** @class */ (function () {
         path.rect(square.cooX, square.cooY, square.width, square.heigth);
         return path;
     };
+    Stage.prototype._drawTriangle = function (triangle) {
+        this.ctx.save();
+        this.ctx.beginPath();
+        this.ctx.translate(this.canvas.width / 2, this.canvas.height / 2);
+        this.ctx.moveTo(0, -triangle.heigth / 2);
+        this.ctx.lineTo(-triangle.side / 2, triangle.heigth / 2);
+        this.ctx.lineTo(triangle.side / 2, triangle.heigth / 2);
+        this.ctx.lineTo(0, -triangle.heigth / 2);
+        this.ctx.lineWidth = this.lineWidth;
+        this.ctx.fill();
+        this.ctx.stroke();
+        this.ctx.closePath();
+        this.ctx.restore();
+        /*
+        this.ctx.strokeStyle = "red";
+        this.ctx.moveTo(100,100);
+        this.ctx.lineTo(50, 50);
+        this.ctx.stroke();
+        */
+    };
+    Stage.prototype._drawStar = function (star) {
+        var rot = Math.PI / 2 * 3;
+        var step = Math.PI / star.spikes;
+        var x = star.cooX;
+        var y = star.cooY;
+        this.ctx.save();
+        this.ctx.strokeStyle = "#FFAB00";
+        this.ctx.beginPath();
+        this.ctx.moveTo(star.cooX, star.cooY - star.outerR);
+        for (var i = 0; i < star.spikes; i++) {
+            x = star.cooX + Math.cos(rot) * star.outerR;
+            y = star.cooY + Math.sin(rot) * star.outerR;
+            this.ctx.lineTo(x, y);
+            rot += step;
+            x = star.cooX + Math.cos(rot) * star.innerR;
+            y = star.cooY + Math.sin(rot) * star.innerR;
+            this.ctx.lineTo(x, y);
+            rot += step;
+        }
+        this.ctx.lineTo(star.cooX, star.cooY - star.outerR);
+        this.ctx.closePath();
+        this.ctx.lineWidth = this.lineWidth;
+        this.ctx.strokeStyle = "#FFAB00";
+        this.ctx.stroke();
+        this.ctx.fillStyle = "#C67C00";
+        this.ctx.fill();
+        this.ctx.restore();
+    };
     Stage.prototype._clear = function () {
         if (this.ctx != null) {
             this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -86,13 +136,23 @@ var Stage = /** @class */ (function () {
         if (shape.type == "square") {
             shape.path = this._drawSquare(shape.obj);
         }
-        this.ctx.beginPath();
-        this.ctx.fillStyle = shape.color.fill;
-        this.ctx.strokeStyle = shape.color.stroke;
-        this.ctx.fill(shape.path);
-        this.ctx.lineWidth = 8;
-        this.ctx.stroke(shape.path);
-        this.ctx.closePath();
+        if (shape.type == "eqTriangle") {
+            this.ctx.fillStyle = shape.color.fill;
+            this.ctx.strokeStyle = shape.color.stroke;
+            this._drawTriangle(shape.obj);
+        }
+        if (shape.type == "star") {
+            this._drawStar(shape.obj);
+        }
+        if (shape.type != "eqTriangle" && shape.type != "star") {
+            this.ctx.beginPath();
+            this.ctx.fillStyle = shape.color.fill;
+            this.ctx.strokeStyle = shape.color.stroke;
+            this.ctx.fill(shape.path);
+            this.ctx.lineWidth = 8;
+            this.ctx.stroke(shape.path);
+            this.ctx.closePath();
+        }
     };
     return Stage;
 }());
--- dynamicShapes.ts
+++ dynamicShapes.ts
@@ -37,13 +37,14 @@ class Triangle {
 class Star {
     cooX: number;
     cooY: number;
-    spikers: number;
+    spikes: number;
     outerR: number;
     innerR: number;
 
-    constructor(x: number, y: number, outerRadio: number, innerRadio: number) {
+    constructor(x: number, y: number, spikes: number, outerRadio: number, innerRadio: number) {
         this.cooX = x;
         this.cooY = y;
+        this.spikes = spikes;
         this.outerR = outerRadio;
         this.innerR = innerRadio;
     }
@@ -83,16 +84,6 @@ class Shape {
         let c = colors[Math.floor(Math.random() * colors.length)];
         return new Color(c.fill, c.stroke, c.name);
     }
-
-    // circle(x: number, y: number, radio: number) {
-    //     return new Circle(x, y, radio);
-    // }
-
-    // _drawCircle(circle) {
-    //     let path = new Path2D();
-    //     path.arc(circle.cooX, circle.cooY, circle.radio, 0, Math.PI * 2);
-    //     return path;
-    // }
 }
 
 class Stage {
@@ -100,7 +91,8 @@ class Stage {
     ctx: any;
     cooX: number;
     cooY: number;
-
+    lineWidth: number = 8;
+    
     constructor(canvas: any, ctx: any) {
         this.canvas = canvas;
         this.ctx = ctx;
@@ -120,6 +112,53 @@ class Stage {
         return path;
     }
 
+    _drawTriangle(triangle: Triangle) {
+        
+        this.ctx.save();
+		this.ctx.beginPath();
+		this.ctx.translate(this.canvas.width/2, this.canvas.height/2);
+		this.ctx.moveTo(0, - triangle.heigth / 2);
+		this.ctx.lineTo(- triangle.side / 2, triangle.heigth / 2);
+		this.ctx.lineTo(triangle.side / 2, triangle.heigth / 2);
+		this.ctx.lineTo(0, - triangle.heigth / 2);
+        this.ctx.lineWidth = this.lineWidth;
+		this.ctx.fill();
+		this.ctx.stroke();
+		this.ctx.closePath();
+        this.ctx.restore();
+    }
+
+    _drawStar(star: Star) {
+    	let rot = Math.PI / 2 * 3;
+		let step = Math.PI /star.spikes;
+		let x = star.cooX;
+		let y = star.cooY;
+
+		this.ctx.save();
+		this.ctx.strokeStyle = "#FFAB00";
+		this.ctx.beginPath();
+	    this.ctx.moveTo(star.cooX, star.cooY - star.outerR)
+	    for (let i = 0; i < star.spikes; i++) {
+	        x = star.cooX + Math.cos(rot) * star.outerR;
+	        y = star.cooY + Math.sin(rot) * star.outerR;
+	        this.ctx.lineTo(x, y)
+	        rot += step
+
+	        x = star.cooX + Math.cos(rot) * star.innerR;
+	        y = star.cooY + Math.sin(rot) * star.innerR;
+	        this.ctx.lineTo(x, y)
+	        rot += step
+	    }
+	    this.ctx.lineTo(star.cooX, star.cooY - star.outerR)
+    	this.ctx.closePath();    
+    	this.ctx.lineWidth = this.lineWidth;
+    	this.ctx.strokeStyle = "#FFAB00";
+    	this.ctx.stroke();
+    	this.ctx.fillStyle = "#C67C00";
+    	this.ctx.fill();
+    	this.ctx.restore();
+	}
+
     _clear() {
         if(this.ctx != null) {
             this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -137,13 +176,25 @@ class Stage {
             shape.path = this._drawSquare(shape.obj);
         }
 
-        this.ctx.beginPath();
-        this.ctx.fillStyle = shape.color.fill;
-        this.ctx.strokeStyle = shape.color.stroke;
-        this.ctx.fill(shape.path);
-        this.ctx.lineWidth = 8;
-        this.ctx.stroke(shape.path);
-        this.ctx.closePath();
+        if(shape.type == "eqTriangle") {
+            this.ctx.fillStyle = shape.color.fill;
+            this.ctx.strokeStyle = shape.color.stroke;            
+            this._drawTriangle(shape.obj);
+        }
+
+        if(shape.type == "star") {
+            this._drawStar(shape.obj);
+        }
+
+        if(shape.type != "eqTriangle" && shape.type != "star") {
+            this.ctx.beginPath();
+            this.ctx.fillStyle = shape.color.fill;
+            this.ctx.strokeStyle = shape.color.stroke;
+            this.ctx.fill(shape.path);
+            this.ctx.lineWidth = 8;
+            this.ctx.stroke(shape.path);
+            this.ctx.closePath();
+        }
     }
 
 }
